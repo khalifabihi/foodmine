86dacb12252dff098c6310f12f476a10f0567da4
Khalifa Bihi <khalifaneymar11@gmail.com>
2023-02-25 19:25:06 +0100

    Commit Changes

--- frontend/src/app/services/food.service.ts
+++ frontend/src/app/services/food.service.ts
@@ -23,6 +23,8 @@ export class FoodService {
   }
 
   getAllFoodsByTag(tag:string):Food[] {
-    return this.getAll().filter(food => food.tags?.includes(tag));
+    return tag === "All"?
+    this.getAll():
+    this.getAll().filter(food => food.tags?.includes(tag));
   }
 }
