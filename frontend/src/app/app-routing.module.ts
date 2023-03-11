01fd1190e6b45d313d00778191581cba88cd5e5e
Khalifa Bihi <khalifaneymar11@gmail.com>
2023-03-11 21:30:14 +0100

    some cart changes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { FoodPageComponent } from './components/pages/food-page/food-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
@@ -6,6 +7,7 @@ const routes: Routes = [
   { path: '', component:HomeComponent },
   { path: 'search/:searchTerm', component:HomeComponent },
   { path: 'tag/:tag', component: HomeComponent },
+  { path: 'food/:id', component: FoodPageComponent },
   { path: '**', redirectTo: '/' }
 ];
 
